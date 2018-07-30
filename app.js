540d2b90835a12ea360b75ade5cd62fcd10bc8c8
LuyenMP <vanhoangluyen@gmail.com>
2018-07-30 08:48:04 +0700

    add object create

--- app.js
+++ app.js
@@ -201,6 +201,8 @@ aArray.forEach(function(item) {
     console.log(item)
 })
 */
+//                      Mark: - Emitter
+/*
 var Emitter = require("./emitter")
 var eventConfig = require("./config").events
 
@@ -220,4 +222,36 @@ for (var score of scores) {
         emitter.emit(eventConfig.BAD_SCORE)
     }
 }
+*/
+//                                  Mark: - Object.create
+/*
+var person = {
+    firstName: "",
+    lastName: "",
+    getFullName: function () {
+        return this.firstName + " " + this.lastName
+    }
+}
+var luyen = Object.create(person)
+luyen.firstName = "Luyen"
+luyen.lastName = "Hoang"
 
+console.log(luyen.getFullName())
+*/
+var EventEmitter = require("events")
+var util = require("util")
+ 
+function Dialog() {
+    this.message = "Hello world !"
+}
+
+util.inherits(Dialog, EventEmitter)
+Dialog.prototype.sayHello = function(data) {
+    console.log(this.message + ": " + data)
+    this.emit("hello", data)
+}
+var dialog = new Dialog()
+dialog.on("hello", function(data) {
+    console.log("Có một lời chào mới !", data)
+})
+dialog.sayHello("Hoang Luyen")
